034ec2617a930b0a1e09c29c537191721bc72db7
88Dand <141179115+88Dand@users.noreply.github.com>
2026-06-05 12:18:10 +0300

    Update install_logviewer.sh

--- install_logviewer.sh
+++ install_logviewer.sh
@@ -10,7 +10,7 @@ LOG_PATH_DEFAULT="/var/www/api/nginx-logs/site.access.log"
 GITHUB_RAW_URL="https://raw.githubusercontent.com/88Dand/NginxLogViewer/main/logviewer.py"
 PORT=8080
 VERSION="2.0"
-
+ 
 # === Цветной вывод ===
 RED='\033[0;31m'
 GREEN='\033[0;32m'
